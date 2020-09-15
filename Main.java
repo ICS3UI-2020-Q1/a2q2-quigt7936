15ca8ce7d7b3da22989e155bf98cac235566aff7
Thomas Quigley <quigt7936@wrdsb.ca>
2020-09-15 12:51:54 +0000

    I completed the question

--- Main.java
+++ Main.java
@@ -1,15 +1,28 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Tells the user which of their two numbers are larger
+ * @author Thomas Quigley
  */
 public class Main {
 
-  /**
-   * The method that is executed when you hit the run button.
-   * @param args the command line arguments
-   */
+ 
   public static void main(String[] args) {
-    // TODO code application logic here
+    Scanner input = new Scanner(System.in);
+
+    // ask the user their numbers and store their values
+    System.out.println("Please enter an integer:");
+    int userInt1 = input.nextInt();
+    System.out.println("Please enter another integer:");
+    int userInt2 = input.nextInt();
+
+    // determines which is larger and tell the user
+    if (userInt1 > userInt2) {
+      System.out.println("The biggest number is " + userInt1 + ".");
+    } else if (userInt1 < userInt2) {
+      System.out.println("The biggest number is " + userInt2 + ".");
+    } else {
+      System.out.println("There is no biggest number.");
+    }
     
   }
 }
